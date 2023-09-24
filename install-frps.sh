a2ea0eb94e9a8fae60a5421b7dc6b6f4ebf58ac9
xiedy00 <xiedy00@vip.qq.com>
2023-09-24 17:16:38 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 ###export###
 export PATH
-export FRPS_VER=0.51.3
+export FRPS_VER=0.48.0
 export FRPS_INIT="https://raw.githubusercontent.com/MvsCode/frps-onekey/master/frps.init"
 export gitee_download_url="https://gitee.com/Mvscode/frps-onekey/releases/download"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
